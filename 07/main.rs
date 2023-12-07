499f98e61857c4c829497a14517695c0f6117bc3
Cris Jr <crisjr@pm.me>
2023-12-07 18:20:35 -0300

    day 7, part 1

--- 07/main.rs
+++ 07/main.rs
@@ -2,21 +2,27 @@ use std::io::{self, BufRead};
 use std::collections::HashMap;
 use std::cmp::Ordering;
 
-struct Constants {
-    card_to_index: HashMap<char, usize>
-}
-
 struct Game {
-    cards: Vec<i32>,
+    cards: Vec<Vec<i32>>,
     bid: i32,
     score: i32,
+    hand: String,
+    card_to_index: HashMap<char, usize>,
+}
+
+fn new_card_to_index() -> HashMap<char, usize> {
+    let mut card_to_index: HashMap<char, usize> = HashMap::new();
+
+    for (i, c) in "23456789TJQKA".chars().enumerate() {
+        card_to_index.insert(c, i);
+    }
+
+    return card_to_index;
 }
 
 fn compare_games(a: &Game, b: &Game) -> Ordering {
-    let mut ia: usize = 0;
-    let mut ha: i32 = 0;
-    let mut ib: usize = 0;
-    let mut hb: i32 = 0;
+    let mut card_a: usize;
+    let mut card_b: usize;
 
     if a.score > b.score {
         return Ordering::Greater;
@@ -24,39 +30,52 @@ fn compare_games(a: &Game, b: &Game) -> Ordering {
         return Ordering::Less;
     }
 
-    for (i, h) in a.cards.clone().into_iter().enumerate() {
-        if h >= ha {
-            ha = h;
-            ia = i;
-        }
-        if b.cards[i] >= hb {
-            hb = b.cards[i];
-            ib = i;
+    for (i, ca) in a.hand.chars().enumerate() {
+        card_a = *a.card_to_index.get(&ca).unwrap();
+        card_b = *b.card_to_index.get(&b.hand.chars().nth(i).unwrap()).unwrap();
+        if card_a > card_b {
+            return Ordering::Greater;
+        } else if card_a < card_b {
+            return Ordering::Less;
         }
     }
 
-    return if ia > ib {
+    return Ordering::Equal;
+}
+
+fn compare_card_scores(a: &Vec<i32>, b: &Vec<i32>) -> Ordering {
+    // a, b are [card, count]
+    return if a[1] > b[1] {
         Ordering::Greater
-    } else {
+    } else if a[1] < b[1] {
         Ordering::Less
+    } else if a[0] > b[0] {
+        Ordering::Greater
+    } else if a[0] < b[0] {
+        Ordering::Less
+    } else {
+        Ordering::Equal
     };
 }
 
-fn parse_game(inlet: String, constants: &Constants) -> Game {
+fn parse_game(inlet: String) -> Game {
     let game: Game;
     let fields: Vec<&str> = inlet.split(" ").collect();
-    let raw_cards: &str = fields[0];
+    let hand: String = fields[0].to_string();
     let bid: i32 = fields[1].parse().unwrap();
-    let mut cards: Vec<i32> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].into_iter().collect();
+    let mut card_count: Vec<i32> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].into_iter().collect();
     let score: i32;
     let mut scores: Vec<i32>;
+    let mut cards: Vec<Vec<i32>> = Vec::new();
+    let mut card_count_pair: Vec<i32>;
+    let card_to_index: HashMap<char, usize> = new_card_to_index();
 
-    for card in raw_cards.chars() {
-        cards[constants.card_to_index[&card]] += 1;
+    for card in hand.chars() {
+        card_count[card_to_index[&card]] += 1;
     }
 
-    // cache score and highest cards
-    scores = cards.clone().into_iter().collect();
+    // creating score cache
+    scores = card_count.clone().into_iter().collect();
     scores.sort();
     scores.reverse();
     score = if scores[0] == 5 {
@@ -75,33 +94,25 @@ fn parse_game(inlet: String, constants: &Constants) -> Game {
         1
     };
 
+    // creating card order cache
+    for (card, count) in card_count.clone().into_iter().enumerate() {
+        card_count_pair = Vec::new();
+        card_count_pair.push(card as i32);
+        card_count_pair.push(count);
+        cards.push(card_count_pair);
+    }
+    cards.sort_by(|a, b| compare_card_scores(a, b));
+    cards.reverse();
+
     game = Game {
         cards,
         bid,
-        score
+        score,
+        hand,
+        card_to_index,
     };
 
     return game;
-}
-
-fn new_card_to_index() -> HashMap<char, usize> {
-    let mut card_to_index: HashMap<char, usize> = HashMap::new();
-
-    card_to_index.insert('2', 0);
-    card_to_index.insert('3', 1);
-    card_to_index.insert('4', 2);
-    card_to_index.insert('5', 3);
-    card_to_index.insert('6', 4);
-    card_to_index.insert('7', 5);
-    card_to_index.insert('8', 6);
-    card_to_index.insert('9', 7);
-    card_to_index.insert('T', 8);
-    card_to_index.insert('J', 9);
-    card_to_index.insert('Q', 10);
-    card_to_index.insert('K', 11);
-    card_to_index.insert('A', 12);
-
-    return card_to_index;
 }
 
 fn main() {
@@ -110,15 +121,11 @@ fn main() {
     let mut inlet: String;
     let mut games: Vec<Game> = Vec::new();
     let mut game: Game;
-    let card_to_index: HashMap<char, usize> = new_card_to_index();
-    let constants = Constants {
-        card_to_index
-    };
 
 	for line in stdin.lock().lines() {
         inlet = line.unwrap();
         if inlet.len() > 0 {
-            game = parse_game(inlet.clone(), &constants);
+            game = parse_game(inlet.clone());
             games.push(game); 
         }
 	}

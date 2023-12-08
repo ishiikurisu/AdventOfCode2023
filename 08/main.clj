808142d0ddb0ac1222813ca70abb2bb89a32d5b8
Cris Jr <crisjr@pm.me>
2023-12-08 12:02:00 -0300

    day 8, part 1, incomplete

---
+++ 08/main.clj
@@ -0,0 +1,42 @@
+(ns advent-of-code.day8
+  (:require [clojure.string :as s]))
+
+(defn read-graph []
+  (loop [graph {}]
+    (let [line (read-line)]
+      (if (-> line count pos?)
+        (let [fields (s/split line #"\W+")]
+          (recur (assoc graph
+                        (nth fields 0) 
+                        [(nth fields 1) (nth fields 2)])))
+        graph))))
+
+(defn traverse-graph [commands graph from memo outlet]
+  (cond
+    (true? (get memo from))
+      nil
+    (= "ZZZ" from)
+      (* outlet (count commands))
+    :else
+      (let [to (reduce (fn [where command]
+                         (let [options (get graph where)]
+                           (if (= \L command)
+                             (first options)
+                             (second options))))
+                       from
+                       commands)]
+        (traverse-graph commands
+                        graph
+                        to
+                        (assoc memo from true)
+                        (inc outlet)))))
+
+(defn main []
+  (let [commands (read-line)
+        _ (read-line)
+        graph (read-graph)]
+    (println (traverse-graph commands graph "AAA" {} 0))
+    ))
+
+(main)
+

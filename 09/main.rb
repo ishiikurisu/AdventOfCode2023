41d36a6c3f182cb37f60d6ab2f7965dccb9cbcd9
Cris Jr <crisjr@pm.me>
2023-12-09 07:49:31 -0300

    day 9, part 1

---
+++ 09/main.rb
@@ -0,0 +1,32 @@
+def all_zeroes? inlet
+  inlet.all?{|x| x == 0}
+end
+
+def find_next_value(inlet)
+  if all_zeroes? inlet then
+    return 0
+  end
+  next_inlet = []
+  for i in 1...inlet.length do
+    next_inlet << inlet[i] - inlet[i-1]
+  end
+  return inlet[-1] + find_next_value(next_inlet)
+end
+
+def main
+  outlet = 0
+  loop do
+    begin
+      line = gets.chomp
+      outlet += find_next_value line.split(" ").collect{|x| x.to_i}
+    rescue
+      break
+    end
+  end
+  puts outlet
+end
+
+if __FILE__ == $0 then
+  main
+end
+

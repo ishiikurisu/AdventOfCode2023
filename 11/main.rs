5ddea15dc0418d94ff1076e81a782e26cc1193cd
Cris Jr <crisjr@pm.me>
2023-12-11 13:02:09 -0300

    day 11, part 2, still wrong, why?

--- 11/main.rs
+++ 11/main.rs
@@ -1,7 +1,9 @@
 use std::io::{self, BufRead};
 
-// static EMPTY_SPACE: i32 = 1000000;
-static EMPTY_SPACE: i32 = 10;
+static EMPTY_SPACE: i64 = 1000000;
+// static EMPTY_SPACE: i64 = 100;
+// static EMPTY_SPACE: i64 = 10;
+// static EMPTY_SPACE: i64 = 2;
 
 fn is_empty(line: String) -> bool {
     for c in line.chars() {
@@ -45,14 +47,14 @@ fn expand_image_vertically(original_image: &Vec<String>) -> Vec<String> {
     return image;
 }
 
-fn calculate_distance(image: &Vec<String>, a: &Vec<i32>, b:&Vec<i32>) -> i32 {
-    let ai: i32 = a[0];
-    let aj: i32 = a[1];
-    let bi: i32 = b[0];
-    let bj: i32 = b[1];
-    let mut outlet: i32 = 0;
-    let mut i: i32;
-    let mut s: i32;
+fn calculate_distance(image: &Vec<String>, a: &Vec<i64>, b:&Vec<i64>) -> i64 {
+    let ai: i64 = a[0];
+    let aj: i64 = a[1];
+    let bi: i64 = b[0];
+    let bj: i64 = b[1];
+    let mut outlet: i64 = 0;
+    let mut i: i64;
+    let mut s: i64;
     let mut c: char;
 
     i = ai;
@@ -64,7 +66,7 @@ fn calculate_distance(image: &Vec<String>, a: &Vec<i32>, b:&Vec<i32>) -> i32 {
     while i != bi {
         c = image[aj as usize].chars().nth(i as usize).unwrap();
         outlet += if c == '+' {
-            EMPTY_SPACE
+            EMPTY_SPACE - 1
         } else {
             1
         };
@@ -80,32 +82,32 @@ fn calculate_distance(image: &Vec<String>, a: &Vec<i32>, b:&Vec<i32>) -> i32 {
     while i != bj {
         c = image[i as usize].chars().nth(bi as usize).unwrap();
         outlet += if c == '+' {
-            EMPTY_SPACE
+            EMPTY_SPACE - 1
         } else {
             1
         };
         i += s;
     }
 
-    return outlet;
+    return outlet + 1;
 }
 
 fn main() {
     let stdin = io::stdin();
     let mut original_image: Vec<String> = Vec::new();
     let image: Vec<String>;
-    let mut galaxies: Vec<Vec<i32>> = Vec::new();
-    let mut galaxy: Vec<i32>;
-    let mut galaxy_a: &Vec<i32>;
-    let mut galaxy_b: &Vec<i32>;
-    let mut count: i32;
+    let mut galaxies: Vec<Vec<i64>> = Vec::new();
+    let mut galaxy: Vec<i64>;
+    let mut galaxy_a: &Vec<i64>;
+    let mut galaxy_b: &Vec<i64>;
+    let mut count: i64;
     let mut inlet: String;
-    let mut outlet: i32 = 0;
+    let mut outlet: i64 = 0;
 
 	for line in stdin.lock().lines() {
         inlet = line.unwrap();
         if is_empty(inlet.clone()) {
-            count = inlet.len() as i32;
+            count = inlet.len() as i64;
             inlet = "".to_string();
             for _ in 0..count {
                 inlet.push('+');
@@ -115,17 +117,14 @@ fn main() {
 	}
 
     image = expand_image_vertically(&original_image);
-    for line in image.clone().into_iter() {
-        println!("{}", line);
-    }
 
     count = 0;
     for (j, line) in image.clone().into_iter().enumerate() {
         for (i, obj) in line.chars().enumerate() {
             if obj == '#' {
                 galaxy = Vec::new();
-                galaxy.push(i as i32);
-                galaxy.push(j as i32);
+                galaxy.push(i as i64);
+                galaxy.push(j as i64);
                 galaxies.push(galaxy);
                 count += 1;
             }
